e00dccbef3c5884128fa6e04c0f4a291a656d8ae
Sofia1074 <164920645+Sofia1074@users.noreply.github.com>
2024-04-15 10:51:45 +0100

    Update sof algorithm.py

--- jogo1/workspace/algorithm.py
+++ jogo1/workspace/algorithm.py
@@ -8,9 +8,13 @@ class AlgorithmPage:
         self.font = pygame.font.SysFont("Calibri", 45, bold=True)
         
         # Define buttons for menu options
-        self.goback = pygame.Rect(580, 70, 160, 100)
-        self.puzzle1 = pygame.Rect((screen_width - 400) // 2-100, (screen_height +300) // 2 - 400, 400, 200)
-        self.puzzle2 = pygame.Rect((screen_width - 400) // 2-100, (screen_height + 950) // 2 - 400, 400, 200)
+      self.goback = pygame.Rect(580, 70, 160, 100)
+        self.BFS = pygame.Rect((screen_width - 400) // 2-100, (screen_height - 50) // 2 - 400, 210, 50)
+        self.DFS = pygame.Rect((screen_width - 400) // 2-100, (screen_height + 130) // 2 - 400, 210, 50)
+        self.Iterative_D = pygame.Rect((screen_width - 400) // 2-100, (screen_height + 310) // 2 - 400, 210, 50)
+        self.Greedy = pygame.Rect((screen_width - 400) // 2-100, (screen_height + 490) // 2 - 400, 210, 50)
+        self.A_star = pygame.Rect((screen_width - 400) // 2-100, (screen_height + 670) // 2 - 400, 210, 50)
+        self.Weighted_A_star = pygame.Rect((screen_width - 400) // 2-100, (screen_height + 850) // 2 - 400, 210, 50)
         self.algoritmback = pygame.image.load("algoritmback.png")
         
     def draw(self, screen):
@@ -20,18 +24,42 @@ class AlgorithmPage:
         scaled_image = pygame.transform.scale(self.algoritmback, (160,100))
         screen.blit(scaled_image, (580, 70))
 
-        pygame.draw.rect(screen, ORANGE, self.puzzle1)
+        pygame.draw.rect(screen, ORANGE, self.BFS)
         # Draw text on buttons
-        start_text = self.font.render("greedy", True, WHITE)
+        start_text = self.font.render("BFS", True, WHITE)
         start_text_pos = start_text.get_rect(center=self.puzzle1.center)
         screen.blit(start_text, start_text_pos)
 
-        pygame.draw.rect(screen, BLUE, self.puzzle2)
+        pygame.draw.rect(screen, ORANGE, self.DFS)
+        # Draw text on buttons
+        start_text = self.font.render("DFS", True, WHITE)
+        start_text_pos = start_text.get_rect(center=self.puzzle1.center)
+        screen.blit(start_text, start_text_pos)
+
+        pygame.draw.rect(screen, ORANGE, self.Iterative_D)
+        # Draw text on buttons
+        start_text = self.font.render("Iterative D", True, WHITE)
+        start_text_pos = start_text.get_rect(center=self.puzzle1.center)
+        screen.blit(start_text, start_text_pos)
+
+        pygame.draw.rect(screen, BLUE, self.Greedy)
+        # Draw text on buttons
+        start_text = self.font.render("Greedy", True, WHITE)
+        start_text_pos = start_text.get_rect(center=self.puzzle2.center)
+        screen.blit(start_text, start_text_pos)
+
+        pygame.draw.rect(screen, BLUE, self.A_star)
         # Draw text on buttons
         start_text = self.font.render("A*", True, WHITE)
         start_text_pos = start_text.get_rect(center=self.puzzle2.center)
         screen.blit(start_text, start_text_pos)
 
+        pygame.draw.rect(screen, BLUE, self.Weighted_A_star)
+        # Draw text on buttons
+        start_text = self.font.render("Weighted A*", True, WHITE)
+        start_text_pos = start_text.get_rect(center=self.puzzle2.center)
+        screen.blit(start_text, start_text_pos)
+    
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -39,8 +67,18 @@ class AlgorithmPage:
                 quit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
-                if self.goback.collidepoint(mouse_pos):
+                if self.BFS.collidepoint(mouse_pos):
+                    return True  # Return True if a puzzle button is clicked
+                if self.DFS.collidepoint(mouse_pos):
+                    return True  # Return True if a puzzle button is clicked    
+                if self.Iterative_D.collidepoint(mouse_pos):
+                    return True  # Return True if a puzzle button is clicked
+                if self.Greedy.collidepoint(mouse_pos):
+                    return True  # Return True if a puzzle button is clicked
+                if self.A_star.collidepoint(mouse_pos):
+                    return True  # Return True if a puzzle button is clicked
+                if self.Weighted_A_star.collidepoint(mouse_pos):
+                    return True  # Return True if a puzzle button is clicked
+                elif self.goback.collidepoint(mouse_pos):
                     return "goback"
         return False
-
-    
